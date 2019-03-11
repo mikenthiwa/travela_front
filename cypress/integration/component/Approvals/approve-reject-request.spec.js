e3c4ccf58d662ff073b964ee3e2624d4daf40166
Akanmu Christopher <akanmuoluwadamilare@yahoo.com>
2019-03-11 19:19:00 +0100

    ch(cypress): write e2e tests for approval page (#646)
    - write e2e tests for the new approval page
    
    [Finishes #164490346]

--- cypress/integration/component/Approvals/approve-reject-request.spec.js
+++ cypress/integration/component/Approvals/approve-reject-request.spec.js
@@ -1,53 +1,88 @@
 const baseAPI = Cypress.env('REACT_APP_API_URL');
-const userDataURL = /api\/v1\/user\/\W+/;
 
 describe('Approval page(Approval or rejection of request by managers)', () => {
-  beforeEach(() => {
-    cy.authenticateUser();
-    cy.server();
-    cy.route('GET', userDataURL).as('getUserDetails');
-    cy.visit('/requests/my-approvals').wait(3000);
-    cy.wait('@getUserDetails').wait(3000);
-  });
+  describe('APPROVAL OF A REQUEST', () => {
+    before(() => {
+      cy.server();
+      cy.route('GET', `${baseAPI}/requests/*`,
+        'fixture:approvals/OpenRequest');
+      cy.route('PUT', `${baseAPI}/approvals/*`,
+        'fixture:approvals/ApprovedRequest');
+      cy.authenticateUser();
+      cy.visit('/requests/my-approvals/CVEgPxX1q');
+    });
   
-  it('should approve a request and toast a successful approval message', () => {
-    cy.get('.request__status--open:first').parent().parent().parent().parent().as('openRow');
-    cy.get('@openRow').find('div.button-outline').click();
-    cy.get('.action-button--approve').click();
-    cy.get('#approve').click();
-    cy.get('.action-button--disabled').contains('reject').should('be.disabled');
-    cy.get('.toast-message')
-      .wait(3000)
-      .should('be.visible')
-      .contains('Request approved successfully');
+    it('should approve a request and toast success message', () => {
+      cy.get('.action-button--approve').click();
+      cy.get('#approve').click();
+      cy.get('.toast-message')
+        .wait(3000)
+        .should('be.visible')
+        .contains('Request approved successfully');
+    });
+
+    it('should check existence of header', () => {
+      cy.get('.header')
+        .contains('REQUEST');
+    });
+
+    it('should check existence of requested by partition', () => {
+      cy.get('.left-pane > :nth-child(1) > :nth-child(1)')
+        .contains('Requested By');
+    });
+
+    it('should certify that approve button is now updated to approved', () => {
+      cy.get('.action-button--approved').contains('approved');
+    });
+
+    it('should certify that reject button is now disabled', () => {
+      cy.get('.action-button--disabled').contains('reject').should('be.disabled');
+    });
   });
 
-  it('should reject a request and toast a successful rejection message', () => {
-    cy.get('.request__status--open:first').parent().parent().parent().parent().as('openRow');
-    cy.get('@openRow').find('div.button-outline').click();
-    cy.get('.action-button--reject').click();     
-    cy.get('#reject').click();
-    cy.get('.action-button--disabled').contains('approve').should('be.disabled');
-    cy.get('.toast-message')
-      .should('be.visible')
-      .contains('Request rejected successfully');
+  describe('SHOULD POST A COMMENT', () => {
+    before(() => {
+      cy.server();
+      cy.route('GET', `${baseAPI}/requests/*`,
+        'fixture:approvals/OpenRequest');
+      cy.route('PUT', `${baseAPI}/approvals/*`,
+        'fixture:approvals/ApprovedRequest');
+      cy.route('POST', `${baseAPI}/comments`,
+        'fixture:approvals/newComment');
+      cy.authenticateUser();
+      cy.visit('/requests/my-approvals/CVEgPxX1q');
+      
+    });
+
+    it('should type in comment box', () => {
+      cy.get('.ql-editor').type('testing you');
+      cy.get('#post-submit').click();
+    });
+
+    it('should certify that a comment has been posted', () => {
+      cy.get(':nth-child(2) > .modal__mdl-icons > .modal__modal2 > .modal__status-update')
+        .contains('testing you');
+    });
   });
 
-  it(`should open request and show approved badge when an approved
-          request is clicked`, () => {
-    cy.get('.request__status--approved:first').parent().parent().parent().parent().as('approvedRow');
-    cy.get('@approvedRow').find('div.button-outline').click();
-    cy.get('.action-button--approved')
-      .should('be.visible')
-      .contains('approved');
-  });
-
-  it(`should open request and show rejected badge when a rejected
-  request is clicked`, () => {
-    cy.get('.request__status--rejected:first').parent().parent().parent().parent().as('rejectedRow');
-    cy.get('@rejectedRow').find('div.button-outline').click();
-    cy.get('.action-button--rejected')
-      .should('be.visible')
-      .contains('rejected');
+  describe('REJECTION OF A REQUEST', () => {
+    before(() => {
+      cy.server();
+      cy.route('GET', `${baseAPI}/requests/*`,
+        'fixture:approvals/OpenRequest');
+      cy.route('PUT', `${baseAPI}/approvals/*`,
+        'fixture:approvals/RejectedRequest');
+      cy.authenticateUser();
+      cy.visit('/requests/my-approvals/CVEgPxX1q');
+    });
+    
+    it('should reject a request and toast success message', () => {
+      cy.get('.action-button--reject').click();
+      cy.get('#reject').click();
+      cy.get('.toast-message')
+        .wait(3000)
+        .should('be.visible')
+        .contains('Request rejected successfully');
+    });
   });
 });
