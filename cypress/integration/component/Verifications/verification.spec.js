3f43032b17c23866ec6266fb1b463c255e93d58a
Bolton <bolton.otieno@gmail.com>
2019-03-25 12:26:40 +0300

    #164292634 write e2e tests on the new travel admin page (#697)
    chore(e2e): write e2e test on the new travel admin page
      - test that  a travel admin can verify a request on the new request page
    [Finishes #164292634]

---
+++ cypress/integration/component/Verifications/verification.spec.js
@@ -0,0 +1,76 @@
+const baseAPI = Cypress.env('REACT_APP_API_URL');
+
+describe('Requests', () => {
+  describe('Travel admin Verification page', () => {
+    before(() => {
+      cy.server();
+      cy.route('GET', `${baseAPI}/approvals?verified=true`,
+        'fixture:requests/requests');
+      cy.authenticateUser();
+      cy.visit('/requests/my-verifications');
+    });
+
+    it('displays the verifications header', () => {
+      cy.get('.PageHeader span.title').contains('VERIFICATIONS');
+    });
+
+    it('displays the All button', () => {
+      cy.get('#all-button').contains('All');
+    });
+
+    it('displays the Pending Verifications button', () => {
+      cy.get('#open-button').contains('Pending Verifications');
+    });
+
+    it('displays the Past Verifications button', () => {
+      cy.get('#past-button').contains('Past Verifications');
+    });
+
+    it('displays the Items per page text', () => {
+      cy.get('.cell-items-per-page-text').contains('Items per page');
+    });
+
+    it('displays pre-populated value of 10 on select drop down', () => {
+      cy.get('.dropdown__input').contains('10');
+    });
+
+    it('displays a component that shows travel request for the admin verification', () => {
+      cy.get(
+        '.mdl-data-table .table__body > :nth-child(1) > :nth-child(2)'
+      ).contains('One-way');
+    });
+
+    it('displays pagination Previous button', () => {
+      cy.get('#previous-button > :nth-child(1)').contains('Previous');
+    });
+
+    it('displays number of pages item ', () => {
+      cy.get('.pagination__items .pagination__page').contains('Page');
+    });
+
+    it('displays pagination Next button', () => {
+      cy.get('#next-button > :nth-child(1)').contains('Next');
+    });
+  });
+
+  describe('Travel admin Single Request page', () => {
+    before(() => {
+      cy.server();
+      cy.route('GET',`${baseAPI}/requests/*`,
+        'fixture:requests/ApprovedRequest'
+      );
+      cy.authenticateUser();
+      cy.visit('/requests/my-verifications/ysH3PCEg2');
+    });
+
+    it('displays the verifications header', () => {
+      cy.get('.header').contains('REQUEST #ysH3PCEg2');
+    });
+
+    it('displays Managers, Budget and Travel label', () => {
+      cy.get('.header > :nth-child(2)').contains('Manager\'s Approval');
+      cy.get('.header > :nth-child(3)').contains('Budget Check');
+      cy.get('.header > :nth-child(4)').contains('Travel Verification');
+    });
+  });
+});
---
+++ cypress/integration/component/Verifications/verify-request.spec.js
@@ -0,0 +1,78 @@
+const baseAPI = Cypress.env('REACT_APP_API_URL');
+
+describe('Verification page(Verification of requests by Travel Admin)', () => {
+  describe('Verification of requests', () => {
+    before(() => {
+      cy.server();
+      cy.route(
+        'GET',
+        `${baseAPI}/requests/*`,
+        'fixture:requests/ApprovedRequest'
+      );
+      cy.route(
+        'GET',
+        `${baseAPI}/checklists/*/submissions`,
+        'fixture:requests/RequestSubmissions'
+      );
+      cy.route(
+        'PUT',
+        `${baseAPI}/requests/*/verify`,
+        'fixture:requests/VerifiedRequest'
+      );
+      cy.authenticateUser();
+      cy.visit('/requests/my-verifications/AWQqIucjm');
+    });
+
+    it('should verify a request and toast success message', () => {
+      cy.get('.action-button--verify').click();
+      cy.get('#verify').click();
+      cy.get('.toast-message')
+        .wait(3000)
+        .should('be.visible')
+        .contains('Verification Successful');
+    });
+  });
+
+  describe('Comment on requests', () => {
+    before(() => {
+      cy.server();
+      cy.route('GET',`${baseAPI}/requests/*`,
+        'fixture:requests/ApprovedRequest'
+      );
+      cy.authenticateUser();
+      cy.visit('/requests/my-verifications/AWQqIucjm');
+    });
+
+    it('displays the Hide comment button', () => {
+      cy.get('.comment-button-text').contains('Hide Comments');
+    });
+
+    it('displays and hides comment editor', () => {
+      cy.get('.comment-button-text').click();
+      cy.get('.ql-editor').should('not.be.visible');
+      cy.get('.comment-button-text').click();
+      cy.get('.ql-editor').should('be.visible');
+    });
+
+    it('displays Comment box Toolbar and Editor', () => {
+      cy.get('.ql-toolbar');
+      cy.get('.ql-editor');
+    });
+
+    describe('Travel Admin Posts a Comment', () => {
+      before(() => {
+        cy.server();
+        cy.route('POST', `${baseAPI}/comments`, 'fixture:requests/NewComment');
+      });
+
+      it('should type in comment box', () => {
+        cy.get('.ql-editor').type('Admin testing comments');
+        cy.get('#post-submit').click();
+      });
+
+      it('should verify that a comment has been posted', () => {
+        cy.get('.modal__status-update').contains('Admin testing comments');
+      });
+    });
+  });
+});
