b688bf24ef73f242f4347eb851ad5f65b48e9c08
Catherine Chepkurui <kathiekim95@gmail.com>
2019-08-26 21:31:47 +0300

    TRAV-213-bug(e2e tests): add new tests and fix failing tests (#1006)
    - write end to end tests for home page
    - fix failing tests for home page
    
    [Fixes TRAV-213]

--- cypress/integration/component/Home/home.spec.js
+++ cypress/integration/component/Home/home.spec.js
@@ -1,45 +1,54 @@
+const base_url = Cypress.env('BASE_URL');
+
 describe('Contents of the Homepage', () => {
 
   before('should set token in cookie', () => {
+    cy.server();
     cy.authenticateUser();
+  });
+
+  it('should visit home page', () => {
     cy.visit('/home');
   });
 
   it('should have a card that displays a welcome message', () => {
-    cy.get('.details p')
-      .as('paragraph');
-
-    cy.get('@paragraph')
-      .should('have.length', 2);
-
+    cy.get('.details p').as('paragraph');
+    cy.get('@paragraph').should('have.length', 2);
     cy.get('.details p.confirm')
       .should('have.class', 'confirm')
       .and('contain', 'Upload details of your travel documents today and avoid last minute hassle.');
   });
 
-  it('should display appropraite message when no team member has trip scheduled', () => {
-    cy.get('.centered-flex div')
-      .should('have.length', 1);
+  it('should display travelling team members card', () => {
+    cy.get('.card-layout--team').should('have.length', 1);
+    cy.get('.card-layout--team > p').contains('Travelling team Members');
   });
 
-  it('should have a request card', () => {
-    cy.get('.card-layout.card-layout--requests')
-      .should('have.length', 1);
+  it('should display requests card', () => {
+    cy.get('.card-layout--requests').should('have.length', 1);
+    cy.get('.card-layout--requests__header > p').contains('Your Requests');
   });
-  
 
-  it('should navigate to the request page', () => {
+  it('should navigate to the travel readiness page onClick Get Started', () => {
     cy.authenticateUser();
-    cy.get('.card-layout--requests__header a')
-      .click();
-    cy.url().should('eq', 'http://localhost:3000/requests');
+    cy.get('.details > a').click({ force: true });
+    cy.url().should('include', '/travel_readiness');
+    cy.url().should('eq', base_url + '/travel_readiness');
   });
 
-  it('should navigate to the travel readiness page', () => {
-    cy.authenticateUser();
-    cy.visit('/home');
-    cy.get('.details a')
-      .click();
-    cy.url().should('eq', 'http://localhost:3000/travel_readiness');
+  it('should display a Passports button', () => {
+    cy.get('#passportButton').contains('Passports');
+  });
+
+  it('should display a visas button', () => {
+    cy.get('#visaButton').contains('Visas');
+  });
+
+  it('should display other documents button', () => {
+    cy.get('#visaButton').contains('Visas');
+  });
+
+  it('should display add passport button', () => {
+    cy.get('#actionButton').contains('Add passport');
   });
 });
